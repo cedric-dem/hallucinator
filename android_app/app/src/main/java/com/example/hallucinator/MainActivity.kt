b5b4fcc03fcdcb922cc7a795668352145f203458
cedric <demuelenaere.cedric@gmail.com>
2025-09-29 14:37:20 +0200

    remove jetpack compose

--- android_app/app/src/main/java/com/example/hallucinator/MainActivity.kt
+++ android_app/app/src/main/java/com/example/hallucinator/MainActivity.kt
@@ -1,47 +1,15 @@
 package com.example.hallucinator
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.hallucinator.ui.theme.HallucinatorTheme
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            HallucinatorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_main)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HallucinatorTheme {
-        Greeting("Android")
+        val greetingText: TextView = findViewById(R.id.greeting_text)
+        greetingText.text = getString(R.string.greeting_message, "Android")
     }
 }
