e7a29b6cd2cbaa988ed20248a85735ffa08498d8
cedric <demuelenaere.cedric@gmail.com>
2025-09-29 16:48:28 +0200

    fix convert to tflite script

--- model_creator/convert_keras_to_tflite.py
+++ model_creator/convert_keras_to_tflite.py
@@ -1,9 +1,20 @@
-
 import tensorflow as tf
 
-model = tf.keras.models.load_model("model_decoder.keras")
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
-#converter.optimizations = [tf.lite.Optimize.DEFAULT]
-tflite_model = converter.convert()
-with open("model_decorder.tflite", "wb") as f:
-    f.write(tflite_model)
+def export_tflite(model_path_keras, model_path_tflite):
+
+    model = tf.keras.models.load_model(model_path_keras, compile=False)
+
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+
+    converter.target_spec.supported_ops = [
+        tf.lite.OpsSet.TFLITE_BUILTINS,
+        tf.lite.OpsSet.SELECT_TF_OPS,
+    ]
+
+    tflite_model = converter.convert()
+    with open(model_path_tflite, "wb") as f:
+        f.write(tflite_model)
+
+export_tflite("model_encoder.keras", "model_encoder.tflite")
+export_tflite("model_decoder.keras", "model_decoder.tflite")
