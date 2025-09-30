f826328a6a3226ae572495360512e4f8ad296053
cedric <demuelenaere.cedric@gmail.com>
2025-09-30 23:12:52 +0200

    clean comment

--- model_creator/config.py
+++ model_creator/config.py
@@ -5,8 +5,9 @@ MODEL_SAVE_FREQUENCY = 10
 
 ENCODED_SIZE = 6272
 
-# '6272_0_small_model', "6272_1_medium_small_model", "6272_2_medium_model","6272_3_big_model", '_0_small_model', "_1_medium_small_model", "_2_medium_model","_3_big_model"
-MODEL_NAME ='6272_0_small_model'
+# '6272_0_small_model', "6272_1_medium_small_model", "6272_2_medium_model","6272_3_big_model"
+# '9604_0_small_model', "9604_1_medium_small_model", "9604_2_medium_model","9604_3_big_model"
+MODEL_NAME ='9604_0_small_model'
 
 BASE_RESULTS_DIR = Path("results")
 RESULTS_DIR = BASE_RESULTS_DIR / MODEL_NAME
